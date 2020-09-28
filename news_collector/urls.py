fc4d05562a5b05270c10508d83cb4fe1f77b2808
fastestmk <fastestmk330@gmail.com>
2020-09-28 09:50:27 +0530

    changed setting urls

--- news_collector/urls.py
+++ news_collector/urls.py
@@ -28,3 +28,15 @@ if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG is False:
+    print('ssssssssssssssssssssss')
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+  
+
+
+
+
