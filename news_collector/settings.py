820a3cebc59ca7ebbe75ec64da17bd0364780e2c
fastestmk <fastestmk330@gmail.com>
2020-09-28 09:31:40 +0530

    changed database settings

--- news_collector/settings.py
+++ news_collector/settings.py
@@ -86,16 +86,12 @@ WSGI_APPLICATION = 'news_collector.wsgi.application'
 #     }
 # }
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "news_collector",
-        'USER': "news_owner",
-        'PASSWORD': "siehf#$tc2.VTH34",
-        'HOST': '',
-        'PORT': '',
-    }
+    'default': "postgres://vapqwtuddvoocu:dc4fa7072cd240cba13b93e4e92ecff470e3bf075eab0b47b33232658417f0aa@ec2-107-22-7-9.compute-1.amazonaws.com:5432/dbu95qaiu69bq4"
 }
+
+
 
 
 # Password validation
@@ -152,3 +148,14 @@ django_heroku.settings(locals())
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "news_collector",
+        'USER': "news_owner",
+        'PASSWORD': "siehf#$tc2.VTH34",
+        'HOST': '',
+        'PORT': '',
+    }
+}
