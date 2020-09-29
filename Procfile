f710973008720fdf546e3674fd9fc944f1590702
fastestmk <fastestmk330@gmail.com>
2020-09-29 17:38:11 +0530

    schdeuled job

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn news_collector.wsgi
+clock: python scripts/get_news.py
--- scripts/get_news.py
+++ scripts/get_news.py
@@ -2,12 +2,18 @@ import os
 import psycopg2
 import requests as re
 from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 # DATABASE_URL = "$(heroku config:get postgresql-triangular-79977 -a news-collector-63298)"  
 DATABASE_URL = "postgres://vapqwtuddvoocu:dc4fa7072cd240cba13b93e4e92ecff470e3bf075eab0b47b33232658417f0aa@ec2-107-22-7-9.compute-1.amazonaws.com:5432/dbu95qaiu69bq4"
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor = conn.cursor()
-print(cursor)
+# print(cursor)
+
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('interval', minutes=5)
 def get_news():
 		url = "https://indianexpress.com/section/world/"
 		data = re.get(url)
@@ -32,4 +38,9 @@ def get_news():
 
 if __name__ == '__main__':
 	get_news()
+	sched.start()
 
+
+
+
+	
