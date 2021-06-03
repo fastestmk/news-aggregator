b8dca4d335165380f3ace693737f2624a8280e22
fastestmk <fastestmk330@gmail.com>
2021-06-03 11:10:10 +0530

    removed env file path from settings

--- news_collector/settings.py
+++ news_collector/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 import django_heroku 
 from pathlib import Path
-from .env import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
